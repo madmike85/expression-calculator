24461810fea17e1b0d0731d4a0235a4d0285e851
Mikhail Shcheglakov <m.shcheglakov@gmail.com>
2019-09-23 21:18:44 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  if (!balancedBrackets(expr)) throw new Error('ExpressionError: Brackets must be paired');
+  if (/\/\s?0/g.test(expr)) throw new Error('TypeError: Devision by zero.');
+
+  const func = new Function('return ' + expr);
+  const result = func();
+
+  return result;
+}
+
+function balancedBrackets(str) {
+  if (!/[()]/g.test(str)) return true;
+  const stack = [];
+  const map = { '(': ')' };
+
+  for (let i = 0; i < str.length; i++) {
+    if (map.hasOwnProperty(str[i])) stack.push(str[i]);
+    else if (/[()]/.test(str[i]) && str[i] !== map[stack.pop()]) return false;
+  }
+
+  if (stack.length !== 0) return false;
+
+  return true;
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator
+};
